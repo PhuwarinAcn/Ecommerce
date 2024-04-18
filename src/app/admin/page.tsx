be27b0a625532d71c83495f89f18c18ab2a9fd1f
Phuwarin <p.maneerat@accenture.com>
2024-04-18 18:12:01 +0700

    Update form

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -6,6 +6,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import db from "@/db/db";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
 
 async function getSalesData() {
   const data = await db.order.aggregate({
@@ -19,14 +20,54 @@ async function getSalesData() {
   };
 }
 
+async function getUserData() {
+  const [userCount, orderData] = await Promise.all([
+    db.user.count(),
+    db.order.aggregate({
+      _sum: { pricePaidInCents: true },
+    }),
+  ]);
+
+  return {
+    userCount,
+    averageValuePeruser:
+      userCount === 0
+        ? 0
+        : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+  };
+}
+
+async function getProductData() {
+  const [activeCount, inactiveCount] = await Promise.all([
+    db.product.count({ where: { isAvailableForPurchase: true } }),
+    db.product.count({ where: { isAvailableForPurchase: false } }),
+  ]);
+  return { activeCount, inactiveCount };
+}
+
 export default async function AdminDashboard() {
-  const salesData = await getSalesData();
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData(),
+  ]);
+
   return (
     <div className="grid gird-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <DashboardCard
         title="Sales"
-        subtitle={salesData.numberOfSales}
-        body={salesData.amount}
+        subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
+        body={formatCurrency(salesData.amount)}
+      />
+      <DashboardCard
+        title="Customer"
+        subtitle={`${formatNumber(userData.averageValuePeruser)} Average Value`}
+        body={formatCurrency(userData.userCount)}
+      />
+      <DashboardCard
+        title="Active Products"
+        subtitle={`${formatNumber(productData.inactiveCount)} Inactive `}
+        body={formatNumber(productData.activeCount)}
       />
     </div>
   );
